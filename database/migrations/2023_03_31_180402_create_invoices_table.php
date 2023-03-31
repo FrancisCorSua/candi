add656b3f54be345fe653432701139913ac63956
Francisco Coruña <franciscorsua95@gmail.com>
2023-03-31 20:39:32 +0100

    Update 2023_03_31_180402_create_invoices_table.php

--- database/migrations/2023_03_31_180402_create_invoices_table.php
+++ database/migrations/2023_03_31_180402_create_invoices_table.php
@@ -14,12 +14,30 @@ return new class extends Migration
 
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
-            $table->integer('numeroFacturas');
             $table->float('importeTotal');
+            $table->float('importePagado');
             $table->date('fechaDesde');
             $table->date('fechaHasta');
             $table->timestamps();
         });
+        
+        $invoices = [
+            ["17.14", "16.0", "2023-03-01", "2023-03-31", "2023-03-01 00:00:00"],
+            ["147.14", "417.14", "2023-03-01", "2023-03-31", "2023-03-01 00:00:00"],
+            ["75.0", "0.0", "2023-01-01", "2023-01-31", "2023-01-01 00:00:00"]
+        ];
+    
+        foreach($invoices as $invoice){
+            DB::table('invoices')->insert(
+                array(
+                    'importeTotal' => $invoice[0],
+                    'importePagado' => $invoice[1],
+                    'fechaDesde' => $invoice[2],
+                    'fechaHasta' => $invoice[3],
+                    'created_at' => $invoice[4]
+                )
+            );
+        }
     }
 
     /**
